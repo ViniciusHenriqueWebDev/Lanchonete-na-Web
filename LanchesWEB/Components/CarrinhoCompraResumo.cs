a4e96049c585eee123760e29e8effbdd3b9eb898
Vinicius <Vinicius@DESKTOP-5SQQP23>
2022-11-09 00:03:06 -0300

    Implementado o método ViewComponents para definir a contagem de itens no carrinho do cliente e a página de contato, com endereço, telefone e e-mail

---
+++ LanchesWEB/Components/CarrinhoCompraResumo.cs
@@ -0,0 +1,43 @@
+﻿using LanchesWEB.Models;
+using LanchesWEB.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanchesWEB.Components
+{
+    public class CarrinhoCompraResumo : ViewComponent
+    {
+        private readonly CarrinhoCompra _carrinhoCompra;
+
+        public CarrinhoCompraResumo(CarrinhoCompra carrinhoCompra)
+        {
+            _carrinhoCompra = carrinhoCompra; 
+        }
+        public IViewComponentResult Invoke()
+        {
+            var itens = _carrinhoCompra.GetCarrinhoCompraItens();
+
+            //var itens = new List<CarrinhoCompraItem>()
+            //{
+            //    new CarrinhoCompraItem(), 
+            //    new CarrinhoCompraItem()    
+            //}; 
+
+            _carrinhoCompra.CarrinhoCompraItens = itens;
+
+            var carrinhoCompraVM = new CarrinhoCompraViewModel
+            {
+                CarrinhoCompra = _carrinhoCompra,
+                CarrinhoCompraTotal = _carrinhoCompra.GetCarrinhoCompraTotal()
+            };
+
+
+
+            return View(carrinhoCompraVM);
+        }
+    }
+}
